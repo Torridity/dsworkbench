597afe11a45bc3e9a431318727fedf3a30b92d98
Torridity.de <Torridity.de@c07c46f2-564f-0410-a54b-ed66933a7292>
2008-06-17 14:30:32 +0000

    Started with attack planner implementation

---
+++ src/de/tor/tribes/util/DSCalculator.java
@@ -0,0 +1,59 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.tor.tribes.util;
+
+import de.tor.tribes.types.Village;
+
+/**
+ *
+ * @author Jejkal
+ */
+public class DSCalculator {
+
+    public static double calculateDistance(Village pSource, Village pTarget) {
+        if ((pSource == null) || (pTarget == null)) {
+            return 0;
+        }
+        return Math.sqrt(Math.pow(pTarget.getX() - pSource.getX(), 2) + Math.pow(pTarget.getY() - pSource.getY(), 2));
+    }
+
+    public static double calculateMoveTimeInMinutes(Village pSource, Village pTarget, int pMinPerField) {
+        return calculateDistance(pSource, pTarget) * pMinPerField;
+    }
+
+    public static double calculateMoveTimeInSeconds(Village pSource, Village pTarget, int pMinPerField) {
+        return calculateDistance(pSource, pTarget) * pMinPerField * 60.0;
+    }
+
+    public static double calculateMoveTimeInHours(Village pSource, Village pTarget, int pMinPerField) {
+        return calculateDistance(pSource, pTarget) * pMinPerField / 60.0;
+    }
+
+    public static String formatTimeInMinutes(double pTime) {
+        double dur = pTime;
+        int hour = (int) Math.floor(dur / 60);
+        dur -= hour * 60;
+        int min = (int) Math.floor(dur);
+        int sec = (int) Math.rint((dur - min) * 60);
+
+        String result = "";
+        if (hour < 10) {
+            result += "0" + hour + ":";
+        } else {
+            result += hour + ":";
+        }
+        if (min < 10) {
+            result += "0" + min + ":";
+        } else {
+            result += min + ":";
+        }
+        if (sec < 10) {
+            result += "0" + sec;
+        } else {
+            result += sec;
+        }
+        return result;
+    }    
+}

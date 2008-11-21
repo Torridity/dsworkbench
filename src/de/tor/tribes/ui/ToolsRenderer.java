a5985367ca5334836181bf8971effee5e611568b
Torridity.de <Torridity.de@c07c46f2-564f-0410-a54b-ed66933a7292>
2008-11-21 15:52:42 +0000

    New Menu graphics

--- src/de/tor/tribes/ui/ToolsRenderer.java
+++ src/de/tor/tribes/ui/ToolsRenderer.java
@@ -20,38 +20,58 @@ import javax.imageio.ImageIO;
  */
 public class ToolsRenderer {
 
-    //private final List<BufferedImage> mToolsImages = new LinkedList<BufferedImage>();
-    private final Hashtable<BufferedImage, List<BufferedImage>> mMenu = new Hashtable<BufferedImage, List<BufferedImage>>();
+    private final List<BufferedImage> mToolsImages = new LinkedList<BufferedImage>();
+    //private final Hashtable<BufferedImage, List<BufferedImage>> mMenu = new Hashtable<BufferedImage, List<BufferedImage>>();
+    private final BufferedImage[] mMenuImages = new BufferedImage[3];
 
     public ToolsRenderer() {
         try {
-            List<BufferedImage> mapSubMenu = new LinkedList<BufferedImage>();
-            mMenu.put(ImageIO.read(new File("./graphics/icons/map_tools.png")), mapSubMenu);
-            mapSubMenu.add(ImageIO.read(new File("./graphics/icons/measure.png")));
-            mapSubMenu.add(ImageIO.read(new File("./graphics/icons/mark.png")));
-            mapSubMenu.add(ImageIO.read(new File("./graphics/icons/tag.png")));
-            mapSubMenu.add(ImageIO.read(new File("./graphics/icons/def.png")));
-            mapSubMenu.add(ImageIO.read(new File("./graphics/icons/booty.png")));
+            mMenuImages[0] = ImageIO.read(new File("./graphics/menu_left.png"));
+            mMenuImages[1] = ImageIO.read(new File("./graphics/menu_center.png"));
+            mMenuImages[2] = ImageIO.read(new File("./graphics/menu_right.png"));
+            mToolsImages.add(ImageIO.read(new File("./graphics/icons/map_tools.png")));
+            mToolsImages.add(ImageIO.read(new File("./graphics/icons/minimap_tools.png")));
+            mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_tools.png")));
+            mToolsImages.add(ImageIO.read(new File("./graphics/icons/general_tools.png")));
 
-            List<BufferedImage> miniMapSubMenu = new LinkedList<BufferedImage>();
-            mMenu.put(ImageIO.read(new File("./graphics/icons/minimap_tools.png")), miniMapSubMenu);
-            miniMapSubMenu.add(ImageIO.read(new File("./graphics/icons/move.png")));
-            miniMapSubMenu.add(ImageIO.read(new File("./graphics/icons/zoom.png")));
-            miniMapSubMenu.add(ImageIO.read(new File("./graphics/icons/camera.png")));
-            List<BufferedImage> attackSubMenu = new LinkedList<BufferedImage>();
-            mMenu.put(ImageIO.read(new File("./graphics/icons/attack_tools.png")), attackSubMenu);
-            attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_axe.png")));
-            attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_ram.png")));
-            attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_snob.png")));
-            attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_spy.png")));
-            attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_light.png")));
-            attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_heavy.png")));
-            attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_sword.png")));
-            List<BufferedImage> miscSubMenu = new LinkedList<BufferedImage>();
-            mMenu.put(ImageIO.read(new File("./graphics/icons/general_tools.png")), miscSubMenu);
-            miscSubMenu.add(ImageIO.read(new File("./graphics/icons/search.png")));
-            miscSubMenu.add(ImageIO.read(new File("./graphics/icons/settings.png")));
-            miscSubMenu.add(ImageIO.read(new File("./graphics/icons/clock.png")));
+            mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_axe.png")));
+        mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_ram.png")));
+        mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_snob.png")));
+        mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_spy.png")));
+        mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_light.png")));
+        mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_heavy.png")));
+        mToolsImages.add(ImageIO.read(new File("./graphics/icons/attack_sword.png")));
+            
+            
+        /*
+        List<BufferedImage> mapSubMenu = new LinkedList<BufferedImage>();
+        mMenu.put(ImageIO.read(new File("./graphics/icons/map_tools.png")), mapSubMenu);
+        mapSubMenu.add(ImageIO.read(new File("./graphics/icons/measure.png")));
+        mapSubMenu.add(ImageIO.read(new File("./graphics/icons/mark.png")));
+        mapSubMenu.add(ImageIO.read(new File("./graphics/icons/tag.png")));
+        mapSubMenu.add(ImageIO.read(new File("./graphics/icons/def.png")));
+        mapSubMenu.add(ImageIO.read(new File("./graphics/icons/booty.png")));
+        
+        List<BufferedImage> miniMapSubMenu = new LinkedList<BufferedImage>();
+        mMenu.put(ImageIO.read(new File("./graphics/icons/minimap_tools.png")), miniMapSubMenu);
+        miniMapSubMenu.add(ImageIO.read(new File("./graphics/icons/move.png")));
+        miniMapSubMenu.add(ImageIO.read(new File("./graphics/icons/zoom.png")));
+        miniMapSubMenu.add(ImageIO.read(new File("./graphics/icons/camera.png")));
+        List<BufferedImage> attackSubMenu = new LinkedList<BufferedImage>();
+        mMenu.put(ImageIO.read(new File("./graphics/icons/attack_tools.png")), attackSubMenu);
+        attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_axe.png")));
+        attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_ram.png")));
+        attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_snob.png")));
+        attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_spy.png")));
+        attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_light.png")));
+        attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_heavy.png")));
+        attackSubMenu.add(ImageIO.read(new File("./graphics/icons/attack_sword.png")));
+        List<BufferedImage> miscSubMenu = new LinkedList<BufferedImage>();
+        mMenu.put(ImageIO.read(new File("./graphics/icons/general_tools.png")), miscSubMenu);
+        miscSubMenu.add(ImageIO.read(new File("./graphics/icons/search.png")));
+        miscSubMenu.add(ImageIO.read(new File("./graphics/icons/settings.png")));
+        miscSubMenu.add(ImageIO.read(new File("./graphics/icons/clock.png")));
+         */
         } catch (Exception e) {
         }
     }
@@ -69,17 +89,12 @@ public class ToolsRenderer {
         g2d.setComposite(c);
          */
         int cnt = 0;
-        Enumeration<BufferedImage> keys = mMenu.keys();
-        while (keys.hasMoreElements()) {
-            BufferedImage i = keys.nextElement();
-            g2d.drawImage(i, cnt * 40, h - 40, null);
-            int cnt2 = 0;
-            List<BufferedImage> subMenu = mMenu.get(i);
-            for (BufferedImage sub : subMenu) {
-                g2d.drawImage(sub, cnt * 40, h - 80 - 40 * cnt2, null);
-                cnt2++;
-            }
+        g2d.drawImage(mMenuImages[0], 0, h - 40, null);
+        for(BufferedImage i : mToolsImages){
+            g2d.drawImage(mMenuImages[1], 20 + cnt * 40, h - 40, null);
+            g2d.drawImage(i, 20 + cnt * 40 + 5, h - 40 + 5, null);
             cnt++;
         }
+        g2d.drawImage(mMenuImages[2], 20 + cnt * 40, h - 40, null);
     }
 }

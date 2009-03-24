f841373cba41c4dc501bb50b84c8791238223192
Torridity.de <Torridity.de@c07c46f2-564f-0410-a54b-ed66933a7292>
2009-03-24 15:25:13 +0000

    Church done

--- src/de/tor/tribes/util/algo/ChurchRangeCalculator.java
+++ src/de/tor/tribes/util/algo/ChurchRangeCalculator.java
@@ -4,13 +4,7 @@
  */
 package de.tor.tribes.util.algo;
 
-import de.tor.tribes.types.Village;
-import de.tor.tribes.ui.DSWorkbenchMainFrame;
-import de.tor.tribes.ui.MapPanel;
-import de.tor.tribes.util.GlobalOptions;
-import de.tor.tribes.util.Skin;
-import java.awt.geom.Ellipse2D;
-import java.awt.geom.GeneralPath;
+import java.awt.Polygon;
 import java.awt.geom.Point2D;
 import java.util.LinkedList;
 import java.util.List;
@@ -21,49 +15,114 @@ import java.util.List;
  */
 public class ChurchRangeCalculator {
 
+    final static Point2D.Double[] translations1 = new Point2D.Double[]{
+        new Point2D.Double(1, 0),
+        new Point2D.Double(1, 1),
+        new Point2D.Double(0, 1),
+        new Point2D.Double(-1, 1),
+        new Point2D.Double(-1, 0),
+        new Point2D.Double(-1, -1),
+        new Point2D.Double(1, -1)
+    };
+    final static Point2D.Double[] translations2 = new Point2D.Double[]{
+        new Point2D.Double(0, 1),
+        new Point2D.Double(-1, 1),
+        new Point2D.Double(-1, 0),
+        new Point2D.Double(-1, -1),
+        new Point2D.Double(1, -1),
+        new Point2D.Double(1, 0),
+        new Point2D.Double(1, 1)
+    };
+    final static Point2D.Double[] translations3 = new Point2D.Double[]{
+        new Point2D.Double(-1, 0),
+        new Point2D.Double(-1, -1),
+        new Point2D.Double(0, -1),
+        new Point2D.Double(1, -1),
+        new Point2D.Double(1, 0),
+        new Point2D.Double(1, 1),
+        new Point2D.Double(0, 1)
+    };
+    final static Point2D.Double[] translations4 = new Point2D.Double[]{
+        new Point2D.Double(0, -1),
+        new Point2D.Double(-1, -1),
+        new Point2D.Double(0, -1),
+        new Point2D.Double(1, -1),
+        new Point2D.Double(1, 0),
+        new Point2D.Double(0, 1),
+        new Point2D.Double(-1, 1)
+    };
+
     //public static GeneralPath getChurchRange(Village pVillage, int pRadius) {
-    public static GeneralPath getChurchRange(int pX, int pY, int pRadius) {
-        //double zoom = DSWorkbenchMainFrame.getSingleton().getZoomFactor();
-        double zoom = 1.0;
-        /*int fieldWidth = GlobalOptions.getSkin().getImage(Skin.ID_DEFAULT_UNDERGROUND, zoom).getWidth(null);
-        int fieldHeight = GlobalOptions.getSkin().getImage(Skin.ID_DEFAULT_UNDERGROUND, zoom).getHeight(null);
-        int x = pVillage.getX();
-        int y = pVillage.getY();
-         */
-        int fieldWidth = 53;
-        int fieldHeight = 38;
-        int x = pX;
-        int y = pY;
-        double diam = 2 * pRadius * fieldWidth + fieldWidth;
-        int horVill = pRadius;
-        int vertVill = 0;
-
+    public static List<Point2D.Double> getChurchRange(int pX, int pY, int pRadius) {
         List<Point2D.Double> villagePos = new LinkedList<Point2D.Double>();
+        //Set InitialStartVillage (x, y-r)
+        //search around village, turning right if one village is in range
+        //x+1 -> x+1,y+1 -> y+1 -> x-1, y+1 -> x-1 -> x-1, y-1 -> y-1 -> x+1,y-1
+        //take first village found and use this as StartVillage
+        //loop until StartVillage = InitialStartVillage
 
-        //add 4 corners of the 3 edges directly
-        //go X/Y -> X+R/Y+R and take village which is outer most
-        //mirror the found villages on y, x and x+y to get all villages and remember the 2 border coordinates
+        Point2D.Double initStart = new Point2D.Double(pX, pY - pRadius);
+        villagePos.add(initStart);
+        calculateSurroundingVillageRecursive(new Point2D.Double(pX, pY), pRadius, initStart, initStart, villagePos, 1);
+        return villagePos;
 
+    }
 
-        for (int i = x; i <= x + pRadius;) {
-            for (int j = y; j < y + pRadius; j++) {
-                double dist = new Point2D.Double(i, j).distance(new Point2D.Double(x, y));
-                if (dist > pRadius) {
-                    villagePos.add(new Point2D.Double(i, j - 1));
-                    break;
-                }
+    private static void calculateSurroundingVillageRecursive(Point2D.Double pCenter, int pRadius, Point2D.Double pInitStart, Point2D.Double pCurrentStart, List<Point2D.Double> pVillages, int pQuadrant) {
+        Point2D.Double[] translationMatrix = null;
+        switch (pQuadrant) {
+            case 1:
+                translationMatrix = translations1;
+                break;
+            case 2:
+                translationMatrix = translations2;
+                break;
+            case 3:
+                translationMatrix = translations3;
+                break;
+            default:
+                translationMatrix = translations4;
+                break;
+        }
+        for (Point2D.Double translation : translationMatrix) {
+            //walk translations
+            Point2D.Double check = new Point2D.Double(pCurrentStart.x + translation.x, pCurrentStart.y + translation.y);
+            if (pCenter.distance(check) <= pRadius) {
+                //got next border point
+                pVillages.add(check);
+                pCurrentStart = check;
+                break;
             }
-            i++;
         }
 
-        for (Point2D.Double p : villagePos) {
-            System.out.println(p);
+        if (pCurrentStart.x == pCenter.x + pRadius && pCurrentStart.y == pCenter.y) {
+            //entering quadrant 2
+           // pVillages.add(pCurrentStart);
+            pQuadrant = 2;
+        } else if (pCurrentStart.x == pCenter.x && pCurrentStart.y == pCenter.y + pRadius) {
+            //entering quadrant 3
+            //pVillages.add(pCurrentStart);
+            pQuadrant = 3;
+        } else if (pCurrentStart.x == pCenter.x - pRadius && pCurrentStart.y == pCenter.y) {
+            //entering quadrant 4
+           // pVillages.add(pCurrentStart);
+            pQuadrant = 4;
         }
 
-        return null;
+        if (pCurrentStart.x == pInitStart.x && pCurrentStart.y == pInitStart.y) {
+            //arrived at start
+            pVillages.remove(pVillages.size() - 1);
+            return;
+        } else {
+            //next recursion
+            calculateSurroundingVillageRecursive(pCenter, pRadius, pInitStart, pCurrentStart, pVillages, pQuadrant);
+        }
     }
 
     public static void main(String[] args) {
+        /*long s = System.currentTimeMillis();
         ChurchRangeCalculator.getChurchRange(500, 500, 4);
+        System.out.println("Dur " + (System.currentTimeMillis() - s));*/
+        Short.parseShort("21323234234");
     }
 }

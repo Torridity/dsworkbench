0dc6bd7fdf14cea9c84e9c89483f10c63160628e
Torridity.de <Torridity.de@c07c46f2-564f-0410-a54b-ed66933a7292>
2009-04-29 14:16:23 +0000

    ROI feature started

--- src/de/tor/tribes/util/ShortcutHandler.java
+++ src/de/tor/tribes/util/ShortcutHandler.java
@@ -5,6 +5,7 @@
 package de.tor.tribes.util;
 
 import java.awt.AWTEvent;
+import java.awt.Toolkit;
 import java.awt.event.AWTEventListener;
 import java.awt.event.KeyEvent;
 
@@ -14,7 +15,10 @@ import java.awt.event.KeyEvent;
  */
 public class ShortcutHandler implements AWTEventListener {
 
-//public static final int ID_SCROLL
+    public static final int ID_SCROLL_LEFT = 0;
+    public static final int ID_SCROLL_UP = 1;
+    public static final int ID_SCROLL_RIGHT = 2;
+    public static final int ID_SCROLL_DOWN = 3;
     private static ShortcutHandler SINGLETON = null;
 
     public static synchronized ShortcutHandler getSingleton() {
@@ -24,18 +28,26 @@ public class ShortcutHandler implements AWTEventListener {
         return SINGLETON;
     }
 
-    public ShortcutHandler() {
+    ShortcutHandler() {
     }
 
     @Override
     public void eventDispatched(AWTEvent event) {
-
         if (((KeyEvent) event).getID() == KeyEvent.KEY_PRESSED) {
             KeyEvent e = (KeyEvent) event;
+            System.out.println("Mods: " + e.getKeyModifiersText(e.getModifiers()));
+            System.out.println("Keys: " + e.getKeyText(e.getKeyCode()));
+        }
+    }
 
-            
-
-
+    public static void main(String[] args) {
+        //Toolkit.getDefaultToolkit().addAWTEventListener(ShortcutHandler.getSingleton(), AWTEvent.KEY_EVENT_MASK);
+      
+        while (true) {
+            try {
+                Thread.sleep(100);
+            } catch (Exception e) {
+            }
         }
     }
 }

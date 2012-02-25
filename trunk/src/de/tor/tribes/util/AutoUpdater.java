b238c305f8d06c079509c742a0aa7a9d340fd188
Torridity.de <Torridity.de@c07c46f2-564f-0410-a54b-ed66933a7292>
2012-02-25 12:05:08 +0000

    Implemented proper live update Removed some old files

--- trunk/src/de/tor/tribes/util/AutoUpdater.java
+++ trunk/src/de/tor/tribes/util/AutoUpdater.java
@@ -5,13 +5,7 @@
 package de.tor.tribes.util;
 
 import de.tor.tribes.util.interfaces.UpdateListener;
-import java.io.DataInputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.URI;
+import java.io.*;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -22,14 +16,20 @@ import java.util.jar.JarEntry;
 import java.util.jar.JarInputStream;
 import java.util.jar.JarOutputStream;
 import java.util.zip.GZIPInputStream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
+import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.FilenameUtils;
 
 /**
  *
  * @author Torridity
  */
 public class AutoUpdater {
+
+    private final static File UPDATES_DIR = new File("./lib/classes");
+
+    static {
+        initialize();
+    }
 
     public static String obtainChangeLog() throws IOException {
         URL u = new URL("http://www.dsworkbench.de/downloads/Update/changelog.gz");
@@ -58,7 +58,15 @@ public class AutoUpdater {
         return buffer.toString();
     }
 
-    public static List<String> getUpdatedResources() throws IOException {
+    public static void initialize() {
+        if (!UPDATES_DIR.exists()) {
+            if (UPDATES_DIR.mkdir()) {
+                System.out.println("Update directory created");
+            }
+        }
+    }
+
+    public static List<String> getUpdatedResources(UpdateListener pListener) throws IOException {
         Properties props = new Properties();
         URL u = new URL("http://www.dsworkbench.de/downloads/Update/hash.props");
         props.load(u.openConnection().getInputStream());
@@ -68,6 +76,8 @@ public class AutoUpdater {
         HashMap<String, Long> existingEntries = new HashMap<String, Long>();
         JarInputStream jarin = null;
         List<String> modified = new ArrayList<String>();
+        int newFiles = 0;
+        int changedFiles = 0;
         try {
             jarin = new JarInputStream(new FileInputStream(currentJar));
             JarEntry entry = jarin.getNextJarEntry();
@@ -77,20 +87,23 @@ public class AutoUpdater {
                 }
                 entry = jarin.getNextJarEntry();
             }
-
-            int newFiles = 0;
-            int changedFiles = 0;
-            
+            jarin.close();
             Set<Object> entries = props.keySet();
 
             for (Object e : entries) {
                 String resource = (String) e;
                 Long newHash = Long.parseLong((String) props.get(resource));
                 Long existingHash = existingEntries.get(resource);
+
+                File existingFile = new File(FilenameUtils.concat(UPDATES_DIR.getPath(), resource));
+                if (existingFile.exists() && !existingFile.isDirectory()) {
+                    existingHash = FileUtils.checksumCRC32(existingFile);
+                }
+
                 if (existingHash == null) {
                     newFiles++;
                     modified.add(resource);
-                } else if (existingHash.compareTo(newHash) != 0) {
+                } else if (newHash.longValue() != existingHash.longValue()) {
                     changedFiles++;
                     modified.add(resource);
                 }
@@ -103,81 +116,100 @@ public class AutoUpdater {
                 }
             }
         }
+        if (pListener != null) {
+            pListener.fireUpdatesFoundEvent(changedFiles, newFiles);
+
+        }
         return modified;
     }
 
     public static void downloadUpdate(List<String> modifiedResources, UpdateListener pListener) throws IOException {
         URL u = new URL("http://www.dsworkbench.de/downloads/Update/core.jar");
-        ZipInputStream zin = null;
-        JarOutputStream jout = null;
+        JarInputStream jin = null;
         try {
-            zin = new ZipInputStream(u.openConnection().getInputStream());
-            jout = new JarOutputStream(new FileOutputStream("./update.jar"));
-            ZipEntry en = zin.getNextEntry();
+            jin = new JarInputStream(u.openConnection().getInputStream());
+            JarEntry en = jin.getNextJarEntry();
             int updateCnt = 0;
             int toUpdate = modifiedResources.size();
             while (en != null) {
                 String resourceName = en.getName();
                 if (modifiedResources.contains(resourceName)) {
-                    ZipEntry n = new ZipEntry(resourceName);
-                    jout.putNextEntry(n);
+                    File outputFile = new File(FilenameUtils.concat(UPDATES_DIR.getPath(), resourceName));
+                    outputFile.getParentFile().mkdirs();
+                    FileOutputStream fout = new FileOutputStream(outputFile);
                     byte[] data = new byte[2048];
+
                     int br = 0;
                     while (br != -1) {
-                        br = zin.read(data);
+                        br = jin.read(data);
                         if (br != -1) {
-                            jout.write(data, 0, br);
+                            fout.write(data, 0, br);
                             data = new byte[2048];
                         }
                     }
-                    jout.closeEntry();
+                    fout.flush();
+                    fout.close();
+
                     updateCnt++;
                     if (pListener != null) {
                         pListener.fireResourceUpdatedEvent(resourceName, 100.0 * updateCnt / toUpdate);
                     }
                     modifiedResources.remove(resourceName);
                 }
-                en = zin.getNextEntry();
+                en = jin.getNextJarEntry();
             }
-
-            jout.finish();
-            jout.flush();
         } finally {
-            if (zin != null) {
+            if (jin != null) {
                 try {
-                    zin.close();
-                } catch (IOException ioe) {
-                }
-            }
-            if (jout != null) {
-                try {
-                    jout.close();
+                    jin.close();
                 } catch (IOException ioe) {
                 }
             }
         }
-
 
         if (pListener != null) {
             if (!modifiedResources.isEmpty()) {
-                pListener.fireUpdateFinishedEvent(false, "Es konnten nicht alle Daten geladen werden");
+                pListener.fireUpdateFinishedEvent(false, "Es konnten nicht alle Dateien geladen werden");
             } else {
                 pListener.fireUpdateFinishedEvent(true, null);
             }
-
         }
-
     }
 
-    
-    
-    
-    
-    
-    
-    
     public static void main(String[] args) throws Exception {
-        System.out.println(obtainChangeLog());
+        //System.out.println(obtainChangeLog());
+        UpdateListener listener = new UpdateListener() {
+
+            @Override
+            public void fireResourceUpdatedEvent(String pResource, double pPercentFinished) {
+                System.out.println("Res: " + pResource);
+            }
+
+            @Override
+            public void fireUpdateFinishedEvent(boolean pSuccess, String pMessage) {
+                System.out.println("DONE!");
+            }
+
+            @Override
+            public void fireUpdatesFoundEvent(int pChangedFiles, int pNewFiles) {
+                System.out.println("Changed: " + pChangedFiles);
+                System.out.println("New: " + pNewFiles);
+            }
+        };
+        long s = System.currentTimeMillis();
+        List<String> modified = AutoUpdater.getUpdatedResources(listener);
+        System.out.println("Updating " + modified.size());
+        if (modified.isEmpty()) {
+            System.out.println("Done!");
+            return;
+        }
+        AutoUpdater.downloadUpdate(modified, listener);
+
+        System.out.println("D " + (System.currentTimeMillis() - s));
+        if (true) {
+            return;
+        }
+
         //////////////////////////////////////////////
         List<File> files = new ArrayList<File>();
         getImages(new File("./graphics"), files);

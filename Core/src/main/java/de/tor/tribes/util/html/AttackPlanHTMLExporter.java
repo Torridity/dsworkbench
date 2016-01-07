d40b45f4445e26b45e528c6a7b0bc3057534294c
Torridity <realstyx@gmx.de>
2016-01-07 09:49:50 +0100

    Update AttackPlanHTMLExporter.java
    Corrected links to image resources

--- Core/src/main/java/de/tor/tribes/util/html/AttackPlanHTMLExporter.java
+++ Core/src/main/java/de/tor/tribes/util/html/AttackPlanHTMLExporter.java
@@ -199,27 +199,27 @@ public class AttackPlanHTMLExporter {
 
             // <editor-fold defaultstate="collapsed" desc="Replace Unit Icons">
             UnitHolder unit = a.getUnit();
-            b = b.replaceAll(UNIT, "<img src=\"http://torridity.de/dsworkbench/export/" + unit.getPlainName() + ".png\">");
+            b = b.replaceAll(UNIT, "<img src=\"http://torridity.de/tools/res/" + unit.getPlainName() + ".png\">");
 
             switch (a.getType()) {
                 case Attack.CLEAN_TYPE: {
-                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/dsworkbench/export/att.png\">");
+                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/tools/res/att.png\">");
                     break;
                 }
                 case Attack.SNOB_TYPE: {
-                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/dsworkbench/export/snob.png\">");
+                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/tools/res/snob.png\">");
                     break;
                 }
                 case Attack.FAKE_TYPE: {
-                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/dsworkbench/export/fake.png\">");
+                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/tools/res/fake.png\">");
                     break;
                 }
                 case Attack.FAKE_DEFF_TYPE: {
-                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/dsworkbench/export/def_fake.png\">");
+                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/tools/res/def_fake.png\">");
                     break;
                 }
                 case Attack.SUPPORT_TYPE: {
-                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/dsworkbench/export/ally.png\">");
+                    b = b.replaceAll(TYPE, "<img src=\"http://torridity.de/tools/res/ally.png\">");
                     break;
                 }
                 default: {

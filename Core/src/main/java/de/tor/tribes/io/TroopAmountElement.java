8e5c782f5b53c6c38ef50148045caee3ba28c966
extremeCrazyCoder <MKich@gmx.at>
2018-03-13 21:01:10 +0100

    Fixed #49

--- Core/src/main/java/de/tor/tribes/io/TroopAmountElement.java
+++ Core/src/main/java/de/tor/tribes/io/TroopAmountElement.java
@@ -81,6 +81,9 @@ public class TroopAmountElement {
             val = parse(pAmount, -1, -1);
             if (val instanceof Double) {
                 dynamicAmount = Integer.toString(((Double) val).intValue());
+            } else if(val instanceof String) {
+                //we can't parse without arguments; curently only needed for 50%
+                dynamicAmount = pAmount;
             }
         }
     }
